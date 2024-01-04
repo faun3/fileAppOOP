b0d6abac2cf82ec1288f27e8699405a133ec2616
faun3 <antonioculcea@gmail.com>
2024-01-04 18:16:20 +0200

    init

--- fileAppCpp/main.cpp
+++ fileAppCpp/main.cpp
@@ -1,14 +1,6 @@
-//
-//  main.cpp
-//  fileAppCpp
-//
-//  Created by aaa on 04.01.2024.
-//
-
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
     std::cout << "Hello, World!\n";
     return 0;
 }
